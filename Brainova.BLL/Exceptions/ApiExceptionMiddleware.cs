4e2d0c5e938bedd4b5072ca312bb8879342dd0f0
Abood <163195187+AbdalruhmanIssa@users.noreply.github.com>
2026-02-17 06:42:56 +0200

    feat: add api exception middleware and business exceptions

---
+++ Brainova.BLL/Exceptions/ApiExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brainova.BLL.Exceptions
+{
+    public class ApiExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ApiExceptionMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ApiException ex)
+            {
+                context.Response.StatusCode = ex.StatusCode;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    success = false,
+                    message = ex.Message
+                });
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    success = false,
+                    message = "Unexpected server error."
+                });
+            }
+        }
+    }
+
+}
